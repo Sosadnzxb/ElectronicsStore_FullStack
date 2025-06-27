f2da608b03f00aac9b88a74a8fe914b6cff31014
Nikita Chaplya <113811092+Sosadnzxb@users.noreply.github.com>
2025-06-27 15:49:45 +0300

    Delete jdbc-app directory

--- jdbc-app/src/main/java/config/DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class DatabaseConfig {
-    public static final String URL = "jdbc:postgresql://localhost:5432/electronics_store_db";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "password";
-}
